5e1fb68cc2a9143a9817e89026ce17deab899dda
Rahul Rathore <rahr@saxobank.com>
2016-12-21 19:22:48 +0530

    Added isUserWon

--- tictactoe.js
+++ tictactoe.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 			y: cell[0].cellIndex
 		};
 		tiktakState[pos.x][pos.y] = -1;
+
 		setTimeout(function() {
 			var done = updateState(tiktakState, pos);
 			if (done.done) {
@@ -33,7 +34,7 @@ $(document).ready(function() {
 
 	function updateState(tiktakState, pos) {
 		var updatedRow = tiktakState[pos.x];
-		var updatedCol = 
+		var updatedCol =
 			[tiktakState[0][pos.y], tiktakState[1][pos.y], tiktakState[2][pos.y]];
 
 		var done = priorityWiseUpdate(tiktakState, tryToWin);
@@ -45,13 +46,12 @@ $(document).ready(function() {
 		if (!done.done) {
 			// row check
 			var updateTo = randomUpdateTo(pos, tiktakState);
-			if(typeof updateTo !== 'undefined'){
+			if (typeof updateTo !== 'undefined') {
 				tiktakState[updateTo.x][updateTo.y] = 1;
 				done.done = true;
 				done.x = updateTo.x;
 				done.y = updateTo.y;
-			}
-			else{
+			} else {
 				done.gameover = true;
 			}
 		}
@@ -126,29 +126,87 @@ $(document).ready(function() {
 
 	function randomUpdateTo(pos, tiktakState) {
 		var cpRow = tiktakState[pos.x];
-		var cpCol = 
+		var cpCol =
 			[tiktakState[0][pos.y], tiktakState[1][pos.y], tiktakState[2][pos.y]];
 		var possibilities = [];
 		var idx = cpRow.lastIndexOf(0);
-		if(idx !== -1){
-			possibilities.push({x: pos.x, y: idx});
+		if (idx !== -1) {
+			possibilities.push({
+				x: pos.x,
+				y: idx
+			});
 		}
 		idx = cpCol.lastIndexOf(0);
-		if(idx !== -1){
-			possibilities.push({x: idx, y: pos.y});
+		if (idx !== -1) {
+			possibilities.push({
+				x: idx,
+				y: pos.y
+			});
 		}
 		var allDiagonals = findDiagonals(tiktakState, 3);
 		var cpDiagonals = diagonalsAcrossPoint(pos, allDiagonals);
-		if(cpDiagonals.length > 0){
-			cpDiagonals.forEach(function(diagonal){
-				var point = diagonal.find(function(pos){
+		if (cpDiagonals.length > 0) {
+			cpDiagonals.forEach(function(diagonal) {
+				var point = diagonal.find(function(pos) {
 					return pos.val === 0;
 				});
-				possibilities.push({x: point.x, y: point.y});
+				possibilities.push({
+					x: point.x,
+					y: point.y
+				});
 			});
 		}
-		var randomIndex = Math.floor(Math.random() * possibilities.length - 1) + 1;		
+		var randomIndex = Math.floor(Math.random() * possibilities.length - 1) + 1;
 		return possibilities[randomIndex];
+	}
+
+	function possibleStraightLines(pos, state, length) {
+		var lines = [];
+
+		var row1 = [],
+			row2 = [],
+			col1 = [],
+			col2 = [],
+			dia1 = [],
+			dia2 = [];
+		for (var i = length - 1; i >= 0; i--) {
+			row.push(state[pos.x + i][pos.y]);
+		}
+	}
+
+	function isGameOver(pos, state, length) {
+		var corrospondingRows = [],
+			corrospondingColumns = []
+		corrospondingDiagonals = [];
+
+		var getRow = function(forward) {
+			var row = [];
+			var operators = {
+				'+': function(x, y) {
+					return x + y;
+				},
+				'-': function(x, y) {
+					return x - y;
+				}
+			};
+			var rowMove = forward ? operators['+'] : operators['-'];
+			for (var i = 0; i < length; i++) {
+				var x = rowMove(pos.x, i);
+				row.push({
+					val: state[x][pos.y],
+					x: x,
+					y: pos.y
+				});
+			}
+			return row;
+		};
+
+		if ((pos.x + length - 1) < state[pos.x].length) {
+			corrospondingRows.push(getRow(true));
+		}
+		if ((pos.x - (length - 1)) >= 0) {
+			corrospondingRows.push(getRow(false));
+		}
 	}
 
 	function highlightWinningLine(tiktakState) {
@@ -158,8 +216,8 @@ $(document).ready(function() {
 			var rowSum = tiktakState[i].sum();
 			if (rowSum === -3 || tiktakState[i].sum() === 3) {
 				rows[i].addClass('blink-green');
-			} 
-			if(rowSum === 3){
+			}
+			if (rowSum === 3) {
 				rows[i].addClass('blink-red');
 			}
 		}
@@ -167,7 +225,7 @@ $(document).ready(function() {
 		for (var i = 0; i < tiktakState[0].length; i++) {
 			var column = [tiktakState[0][i], tiktakState[1][i], tiktakState[2][i]];
 			if (column.sum() === -3 || column.sum() === 3) {
-				var blinkClass = column.sum() === 3? 'blink-red': 'blink-green';
+				var blinkClass = column.sum() === 3 ? 'blink-red' : 'blink-green';
 				$($(rows[0]).children()[i]).addClass(blinkClass);
 				$($(rows[1]).children()[i]).addClass(blinkClass);
 				$($(rows[2]).children()[i]).addClass(blinkClass);
@@ -175,14 +233,14 @@ $(document).ready(function() {
 		}
 		var diagonal1 = [tiktakState[0][0], tiktakState[1][1], tiktakState[2][2]];
 		var diagonal2 = [tiktakState[2][0], tiktakState[1][1], tiktakState[0][2]];
-		if(diagonal1.sum() === 3 || diagonal1.sum() === -3){
-			var blinkClass = diagonal1.sum() === 3? 'blink-red': 'blink-green';
+		if (diagonal1.sum() === 3 || diagonal1.sum() === -3) {
+			var blinkClass = diagonal1.sum() === 3 ? 'blink-red' : 'blink-green';
 			$($(rows[0]).children()[0]).addClass(blinkClass);
 			$($(rows[1]).children()[1]).addClass(blinkClass);
 			$($(rows[2]).children()[2]).addClass(blinkClass);
 		}
-		if(diagonal2.sum() === 3 || diagonal2.sum() === -3){
-			var blinkClass = diagonal2.sum() === 3? 'blink-red': 'blink-green';
+		if (diagonal2.sum() === 3 || diagonal2.sum() === -3) {
+			var blinkClass = diagonal2.sum() === 3 ? 'blink-red' : 'blink-green';
 			$($(rows[2]).children()[0]).addClass(blinkClass);
 			$($(rows[1]).children()[1]).addClass(blinkClass);
 			$($(rows[0]).children()[2]).addClass(blinkClass);
@@ -190,56 +248,91 @@ $(document).ready(function() {
 	}
 
 	function diagonalsAcrossPoint(point, allDiagonals) {
-		return allDiagonals.filter(function(item){
-			return item.includes(item.find(function(val){
+		return allDiagonals.filter(function(item) {
+			return item.includes(item.find(function(val) {
 				return val.x === point.x && val.y === point.y;
 			}));
 		});
 	}
-	
-	function findRowsInFullRow(arr, rowLength) {
-		var rows = [];
-		for (var i = 0; i < arr.length; i++) {
-			var row = [];
-			for (var j = 0; j < rowLength; j++) {
-				var x = i + j;
-				if (x < arr.length) {
-					row.push({
-						'val': arr[x],
-						'x': x
-					});
-				}
-			}
-			if (row.length === rowLength) {
-				rows.push(row);
-			}
-		}
-		return rows;
+
+	var findWinRow = function winRow(inArray, winLength) {
+		var at = -1,
+			winningSum = winLength * -1;
+
+		inArray.sliding(winLength, undefined, function (arr, idx) {
+			if(arr.sum() !== winningSum){ return; }
+			at = idx;
+			return true;
+		});
+		return at;
 	}
-	
-	function findDiagonals(tiktakState, daigonalLength){
+	function isUserWon(state, winLength, pos) {
+		var won;
+		var winningSum = winLength * -1;
+		var start = Math.max(0, pos.y  - (winLength - 1)),
+			end = pos.y + winLength;
+		//For row
+		var rowMadeAt = findWinRow(state[pos.x].slice(start, end), winLength);
+		console.log(rowMadeAt, start, end);
+		if (rowMadeAt >= 0) {
+			won = {
+				from: { x: pos.x, y: rowMadeAt -1 },
+				to: { x: pos.x, y: rowMadeAt + winLength -1 }
+			}
+			return won;
+		}
+		//For column
+		var start = Math.max(0, pos.x  - (winLength - 1)),
+			end = pos.x + winLength;
+		
+		var incolumn = state.map(function (row, idx) {
+			return row[pos.y];
+		}).slice(start, end);
+		console.log(incolumn);
+		var colMadeAt = findWinRow(incolumn, winLength);
+
+		if(colMadeAt >= 0){
+			won = {
+				from: { x: colMadeAt - 1, y: pos.y },
+				to: { x: colMadeAt + winLength - 1, y: pos.y }
+			}
+			return won;
+		}
+		return won;
+	}
+
+	function findDiagonals(state, daigonalLength) {
 		var diagonals = [];
-		if(tiktakState.length !== tiktakState[0].length){
+		if (state.length !== state[0].length) {
 			throw "Two dimentional array must be square.";
 		}
-		for (var i = 0; i < tiktakState.length; i++) {
-			for(var j = 0; j < tiktakState[0].length; j++){
-				var diagonal1 = [], x, y;
-				var diagonal2 = [], w, z;
-				for(var k = 0; k < daigonalLength; k++){
+		for (var i = 0; i < state.length; i++) {
+			for (var j = 0; j < state[0].length; j++) {
+				var diagonal1 = [],
+					x, y;
+				var diagonal2 = [],
+					w, z;
+				for (var k = 0; k < daigonalLength; k++) {
 					x = i + k;
 					y = j + k;
 					w = i + (daigonalLength - 1) - k;
 					z = j + k;
-					if(x < tiktakState.length && y < tiktakState[0].length){
-						diagonal1.push({'val':tiktakState[x][y], 'x':x, 'y':y});
+					if (x < state.length && y < state[0].length) {
+						diagonal1.push({
+							'val': state[x][y],
+							'x': x,
+							'y': y
+						});
 					}
-					if(w >= 0 && z >=0 && z < tiktakState.length
-						&& w < tiktakState[0].length){
-						diagonal2.push({'val':tiktakState[w][z], 'x':w, 'y':z});
+					if (w >= 0 && z >= 0 && z < state.length && w < state[0].length) {
+						diagonal2.push({
+							'val': state[w][z],
+							'x': w,
+							'y': z
+						});
 					}
 				}
-				if(x < tiktakState.length && y < tiktakState[0].length){
+				if (x < state.length && y < state[0].length) {
 					diagonals.push(diagonal1);
 					diagonals.push(diagonal2);
 				}
@@ -247,44 +340,45 @@ $(document).ready(function() {
 		}
 		return diagonals;
 	}
-	function isGameOver(pos, state, length){
-		var corrospondingRows = [],
-			corrospondingColumns = []
-			corrospondingDiagonals = [];
 
-		var getRow = function (forward) {
-			var row = [];
-			var operators = {
-			    '+' : function(x, y) {
-			        return x + y;
-			    },
-			    '-' : function(x, y) {
-			        return x - y;
-			    }
-			};
-			var rowMove = forward ? operators['+']: operators['-'];
-			for(var i = 0; i < length; i++){
-				var x = rowMove(pos.x, i);
-				row.push({val:state[x][pos.y], x:x, y:pos.y});
-			}
-			return row;
-		};
-
-		if((pos.x + length - 1) < state[pos.x].length){
-			corrospondingRows.push(getRow(true));
-		}
-		if((pos.x - (length - 1)) >= 0){
-			corrospondingRows.push(getRow(false));
-		}
-	}
+	 /**
+     * Adds all elements of array and skips non numeric values
+     * @param {number[]} array the input array.
+     * @returns {number}
+     */
 	Array.prototype.sum = function() {
 		var arr = this;
 		return arr.reduce(function(sum, item) {
-			return sum + item;
+			if(item && !isNaN(parseFloat(item))){
+				return sum + item;
+			}
+			else{
+				return sum;
+			}
 		}, 0);
 	};
 	Array.prototype.lastEmptyPlace = function() {
 		var arr = this;
 		return arr.lastIndexOf(0);
 	};
+
+	Array.prototype.sliding = function(size, defaultValue, iterator) {
+		var arr = this;
+        var window = [];
+        var output = [];
+        arr.forEach(function(num, index) {
+            window.push(num);
+            if (window.length === size) {
+                output.push(iterator(window, index)); // passing starting index of the sliding window
+                window = window.tail();
+            } else {
+                output.push(defaultValue);
+            }
+        });
+        return output;
+    };
+    Array.prototype.tail = function () {
+    	var arr = this;
+    	return arr.slice(1, arr.length);
+    }
 });
